df7d0c2e9377f86e212860cdc1a842822e9eb0d5
Ella Gading <ellagading@ellas-mbp.myfiosgateway.com>
2022-02-23 21:54:03 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,22 +1,23 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 // More code here in a moment
 // GET /places
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'Tacos',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Mexican',
-        pic: '/images/moretacos.jpg'
-      }, {
-          name: 'P & C Cafe',
-          city: 'Phoenix',
-          state: 'AZ',
-          cuisines: 'Coffee, Bakery, Plants',
-          pic: '/images/coffee.jpg'
-      }]
-    res.render('places/index', { places })
+ //   let places = [{
+ //       name: 'Tacos',
+ //       city: 'Seattle',
+ //       state: 'WA',
+ //       cuisines: 'Mexican',
+ //       pic: '/images/moretacos.jpg'
+ //     }, {
+ //        name: 'P & C Cafe',
+ //         city: 'Phoenix',
+ //         state: 'AZ',
+ //         cuisines: 'Coffee, Bakery, Plants',
+ //         pic: '/images/coffee.jpg'
+ //     }]
+res.render('places/index', { places })
 })
 // GET /places/new
 router.get('/new', (req, res) => {
@@ -24,8 +25,19 @@ router.get('/new', (req, res) => {
 })
 // POST /places 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
-})
+    // console.log(req.body)
+    if (!req.body.pic) {
+      // Default image if one is not provided
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+    places.push(req.body)
+    res.redirect('/places')
+  })
 
 module.exports = router
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Tacos',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Mexican',
+    pic: '/images/moretacos.jpg'
+}, {
+    name: 'P & C Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery, Plants',
+    pic: '/images/coffee.jpg'
+}]
