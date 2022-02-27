c6a2f47276432dc5c38ddb52df4d25178c52e2e0
Ella Gading <ellagading@ellas-mbp.myfiosgateway.com>
2022-02-26 22:21:47 -0500

    Adding Edit and Delete button

--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const methodOverride = require('method-override')
 
 // Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
