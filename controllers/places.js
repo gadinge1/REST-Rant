59ef0926b9931d3861a14396f5ac1434a73d4c30
Ella Gading <ellagading@ellas-mbp.myfiosgateway.com>
2022-02-26 22:29:03 -0500

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -52,5 +52,17 @@ router.get('/:id', (req, res) => {
     res.render('places/show', { place: places[id], id })
   }
 })
-
+router.delete('/places/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
 module.exports = router
