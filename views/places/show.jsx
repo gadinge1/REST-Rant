55244bec9722b9993524010ed294df40ff7751bc
Ella Gading <ellagading@ellas-mbp.myfiosgateway.com>
2022-07-06 23:54:13 -0400

    updated

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,37 +3,49 @@ const Def = require('../default')
 const new_form = require('./new')
 
 function show (data) {
-    let comments = (
-      <h3 className="inactive">
-        No comments yet!
+  let comments = (
+    <h3 className="inactive">
+      No comments yet!
+    </h3>
+  )
+  let rating = (
+    <h3 className="inactive">
+      Not yet rated
+    </h3>
+  )
+  if (data.place.comments.length) {
+    let sumRatings = data.place.comments.reduce((tot, c) => {
+      return tot + c.stars
+    }, 0)
+    let averageRating = Math.round(sumRatings / data.place.comments.length)
+    let stars = ''
+    for (let i = 0; i < averageRating; i++) {
+      stars += '⭐️'
+    }
+    rating = (
+      <h3>
+        {starts} stars
       </h3>
     )
-    if (data.place.comments.length) {
-      comments = data.place.comments.map(c => {
-        return (
-          <div className="border">
-            <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😻'}</h2>
-            <h4>{c.content}</h4>
-            <h3>
-              <stong>- {c.author}</stong>
-            </h3>
-            <h4>Rating: {c.stars}</h4>
-          </div>
-        )
-      })
-    }
-    return (
-        <Def>
-          <main>
-            <div className="row">
+  }
+  return (
+      <Def>
+        <main>
+          <div className="row">
+            ...
+            <div className="col-sm-6">
+              <h1>{ data.place.name }</h1>
+              <h2>
+                Rating
+              </h2>
+              {rating}
+              <br />
               ...
             </div>
-            <hr />
-            <h2>Comments</h2>
-            {comments}
-          </main>
-        </Def>
+          </div>
+        </main>
+      </Def>
     )
-}
+  }
 
 module.exports = show
